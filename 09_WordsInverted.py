469129931796fd739c8fffb285985e6ff40383b2
[Ivan Valadez] <[ivaladez@luxoft.com]>
2019-08-27 08:46:02 -0500

    9.1 Commit, addition of comment to 09_WordsInverted

--- 09_WordsInverted.py
+++ 09_WordsInverted.py
@@ -3,18 +3,25 @@ def words_inverter(sentence="hola mundo"):
     inverted_list = []
     aux_sentence = ""
     count = 0
+    # Loop, to keep tracking the position in the array(sentence)
     while len(sentence) > count:
+        # Check for a letter and keep there until find a non-alphanumeric character
         while sentence[count].isalpha():
+            # Add letter by letter to a string
             aux_sentence += sentence[count]
+            # To avoid exeed the size of the array
             if len(sentence) == (count + 1):
                 break
             else:
                 count += 1
+        # Add the word learnt into a list
         inverted_list.append(aux_sentence)
+        # Clean the variable to store the word
         aux_sentence = ""
-
+        # Continue to the next position
         count += 1
-
+    # Print in reverse the list learnt
+    # [Begining position:End position:Increment of count]
     print(inverted_list[-1:-(len(inverted_list)+1):-1])
 
 
